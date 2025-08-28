d37cb8c5f688a7b73093014b7b29c6ec52174ad8
Inu Enike <inuenike@gmail.com>
2025-08-28 10:28:56 +0100

    commented middleware function

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-// // middleware.ts
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//   console.log("🔄 Middleware dey run on:", req.nextUrl.pathname);
-//   const token = req.cookies.get("token"); // check cookie
+export function middleware(req: NextRequest) {
+  console.log("🔄 Middleware dey run on:", req.nextUrl.pathname);
+  // const token = req.cookies.get("token"); // check cookie
 
-//   // If no token, redirect away
-//   if (!token) {
-//     return NextResponse.redirect(new URL("/auth/login", req.url));
-//   }
+  // // If no token, redirect away
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/auth/login", req.url));
+  // }
 
-//   return NextResponse.next();
-// }
+  // return NextResponse.next();
+}
 
 // export const config = {
 //   matcher: ["/((?!_next|favicon.ico|auth).*)"],
